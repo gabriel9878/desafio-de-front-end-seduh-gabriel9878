88eabfa1c8b8c479ef9910ed107ee70f23a17e9d
Gabriel Souza <53787151+gabriel9878@users.noreply.github.com>
2026-01-15 17:17:32 -0300

    Atualiza build estático do app

--- .next/dev/static/chunks/app_page_tsx_a8c03fea._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_4e73a89f._.js"
-],
-    source: "dynamic"
-});
